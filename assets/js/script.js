1d3994dd1edaccb1f0d34e305dfe4146edb5bcbc
Khant Phone <khantphone333@gmail.com>
2021-08-03 16:27:53 +0630

    contact section , activities section , popular places section and footer section completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,39 @@
-/*wowjs*/
-new WOW().init();
+/*wowJs*/
+
+	new WOW().init();
+
+
+/*toTop*/
+
+  const toTop = document.querySelector(".to-top");
+  window.addEventListener("scroll",() => {
+
+    if (window.pageYOffset > 100) {      
+        toTop.classList.add("active");
+    }else{
+        toTop.classList.remove("active");
+     }
+    });
+
+/*swiperJs*/
+
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      centeredSlides: true,
+      spaceBetween: 30,
+      grabCursor: true,
+      loop:true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets : true,
+        
+      },
+
+      loop:true,
+       autoplay: {
+        delay:1400 ,
+        disableOnInteraction: false,
+
+      },
+    });
